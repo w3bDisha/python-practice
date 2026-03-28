2debcd76a47b6d5b5ae0fa56d6df39eb3a970578
Disha Singh <missdisha3728@gmail.com>
2026-03-29 00:20:53 +0530

    Add files via upload
    This folder contains Python practice programs based on:
    - Variables
    - Data Types
    
    These programs helped me understand basic Python concepts.

---
+++ Variables and data types/3rd.py
@@ -0,0 +1,4 @@
+a = float(input("enter first number: "))
+b = float(input("enter sec number: "))
+
+print("average is: ",(a+b)/2)
---
+++ Variables and data types/4th.py
@@ -0,0 +1,4 @@
+a = int(input("Enter first num: "))
+b = int(input("Enter sec num: "))
+
+print(a>=b)
