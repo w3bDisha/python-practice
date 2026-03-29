5e2de03c1a5c76eb0dcc818c7e0cfd9ac327ea96
Disha Singh <missdisha3728@gmail.com>
2026-03-29 19:30:00 +0530

    Rename 2nd.py to float_datatype.py

--- Variables and data types/2nd.py
+++
@@ -1,3 +0,0 @@
-side = float(input("enter square side: "))
-
-print("area = ", side*side) #side ** 2 bhi likh skte h same answer dega
---
+++ Variables and data types/float_datatype.py
@@ -0,0 +1,3 @@
+side = float(input("enter square side: "))
+
+print("area = ", side*side) #side ** 2 bhi likh skte h same answer dega
