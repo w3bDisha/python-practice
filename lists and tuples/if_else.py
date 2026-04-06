7b460e089333b176c7ec2fd65ba1250db97674cf
Disha Singh <missdisha3728@gmail.com>
2026-04-06 19:13:01 +0530

    Delete lists and tuples directory

--- lists and tuples/if_else.py
+++
@@ -1,9 +0,0 @@
-list = [1,2,1]
-
-copy_list = list.copy()
-copy_list.reverse()
-
-if(copy_list == list):
-    print("It is a palindrome")
-else:
-    print("It is not a palindrome")
--- lists and tuples/tuple_sort.py
+++
@@ -1,4 +0,0 @@
-grade = ["C","D","A","A","B","B","A"]
-grade.sort()
-print(grade)
-
