6ecc1c310b301b912da984e86f1cf51d88b4dc10
Disha Singh <missdisha3728@gmail.com>
2026-04-06 19:14:12 +0530

    Add files via upload

---
+++ 03_lists_and_tuples/list_append.py
@@ -0,0 +1,10 @@
+movie = []
+movie1 = input("Enter your 1st favourite movie:")
+movie2 = input("Enter your 2nd favourite movie:")
+movie3 = input("Enter your 3rd favourite movie:")
+
+movie.append(movie1)
+movie.append(movie2)
+movie.append(movie3)
+
+print(movie)
