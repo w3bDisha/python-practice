6bba1bc3eac24e46fd1caf182bbe4766e635da59
Disha Singh <missdisha3728@gmail.com>
2026-04-06 19:15:14 +0530

    Add files via upload

---
+++ 05_Loops.py/for_loop.py
@@ -0,0 +1,7 @@
+#for even numbers
+for i in range(2, 100, 2):
+    print(i)
+
+#for odd numbers
+for i in range(1, 100, 2):
+    print(i)
---
+++ 05_Loops.py/while_loop.py
@@ -0,0 +1,8 @@
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+#traverse
+
+idx = 0
+while idx < len(nums):
+    print(nums[idx])
+    idx += 1
