149fa371ae66b38dc884e942099bcfb170477178
Disha Singh <missdisha3728@gmail.com>
2026-04-14 14:08:39 +0530

    Add comments to str_count.py for clarity
    Added comments explaining the purpose of the code.

--- 02_strings_and_conditional_stats/str_count.py
+++ 02_strings_and_conditional_stats/str_count.py
@@ -1,3 +1,7 @@
 str = ("$Hi , $I am Disha Singh. $From nashik , $i am a coder.")
 
 print("There are",str.count("$"),"dollar signs",)
+
+
+#this program uses a function of string of count 
+#This code is to count the dollar sign written above
