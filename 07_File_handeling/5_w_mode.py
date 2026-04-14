551496faccb7e2e3f3dc1ee71c725887989a4307
Disha Singh <missdisha3728@gmail.com>
2026-04-14 14:21:11 +0530

    Add files via upload

---
+++ 07_File_handeling/5_w_mode.py
@@ -0,0 +1,4 @@
+with open("practice.txt", "w") as file:
+    file.write("Hello everyone!\nwe are learning File I/O\n")
+    file.write("using java\n")
+    file.write("I like programming in java\n")
